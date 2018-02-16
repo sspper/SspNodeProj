ea91d088ed7e9d4acbe7a50e18170d1acaf87317
Shiva Salugu <shiva.salugu@sumtotalsystems.com>
2018-02-16 18:35:53 +0530

    script addedd

---
+++ jenkins/kube_config.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+API_SERVER="https://api.k8s.sspcloudpro.co.in"
+TOKEN="45guqmXEt3BBgrcdqAAyUtwyr8Rbnmy3"
+CLUSTER_NAME="k8s.sspcloudpro.co.in"
+
+kubectl config set preferences.colors true
+kubectl config set-cluster ${CLUSTER_NAME} --server=${API_SERVER} --insecure-skip-tls-verify=true
+kubectl config set-credentials ${CLUSTER_NAME} --token=${TOKEN}
+kubectl config set-context develop-paris --cluster=${CLUSTER_NAME} --user=${CLUSTER_NAME} --namespace=develop
+# To test that configuration works
+kubectl get po --context="k8s.sspcloudpro.co.in" --namespace develop
+kubectl config get-contexts --namespace develop
